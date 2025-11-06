881a12e8547315bba370c927c30cf8f6c3ca8edd
Sergiu Gherasim <sergeras4@gmail.com>
2025-11-06 19:28:04 +0200

    feat: add 3 Display objects to Main.java & compare them

--- OOP_LAB4/Main.java
+++ OOP_LAB4/Main.java
@@ -1,5 +1,17 @@
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello, World!");
+    var display1 = new Display(1920, 1080, 401, "Phone A");
+    var display2 = new Display(2560, 1440, 515, "Phone B");
+    var display3 = new Display(1920, 1080, 401, "Phone C");
+
+
+    display1.compareWithMonitor(display2);
+    display1.compareWithMonitor(display3);
+    System.out.println();
+    display2.compareWithMonitor(display1);
+    display2.compareWithMonitor(display3);
+    System.out.println();
+    display3.compareWithMonitor(display1);
+    display3.compareWithMonitor(display2);
   } 
 }
