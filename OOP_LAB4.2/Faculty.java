eb5181c42176c256672495297f0f3938f12e7a4f
Sergiu Gherasim <sergeras4@gmail.com>
2025-11-10 13:49:13 +0200

    feat: add Faculty class

---
+++ OOP_LAB4.2/Faculty.java
@@ -0,0 +1,25 @@
+import java.util.List;
+
+public class Faculty {
+  private final String name;
+  private final String abbreviation;
+  private final List<Student> students;
+  private final StudyField studyField;
+
+  public Faculty(
+    String name,
+    String abbreviation,
+    List<Student> students,
+    StudyField studyField
+  ) {
+    this.name = name;
+    this.abbreviation = abbreviation;
+    this.students = students;
+    this.studyField = studyField;
+  }
+
+  public String getName() { return name; }
+  public String getAbbreviation() { return abbreviation; }
+  public List<Student> getStudents() { return students; }
+  public StudyField getStudyField() { return studyField; }
+}
