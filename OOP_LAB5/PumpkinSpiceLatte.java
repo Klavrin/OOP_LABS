5211f15c440ff0a15c091219ad9345a91abb1968
Sergiu Gherasim <sergeras4@gmail.com>
2025-12-02 08:49:39 +0200

    feat: add printCoffeeDetails method to PumpkinSpiceLatte class

--- OOP_LAB5/PumpkinSpiceLatte.java
+++ OOP_LAB5/PumpkinSpiceLatte.java
@@ -1,5 +1,6 @@
 public class PumpkinSpiceLatte extends Coffee {
   int mlOfMilk;
+  int mlOfpumpkingSpice;
 
   public PumpkinSpiceLatte(Intensity intensity, int mlOfMilk) {
     super(intensity, "Pumpkin Spice Latte");
@@ -8,5 +9,12 @@ public class PumpkinSpiceLatte extends Coffee {
 
   public int getMlOfMilk() {
     return mlOfMilk;
+  }
+
+  @Override
+  public void printCoffeeDetails() {
+    super.printCoffeeDetails();
+    System.out.println("Milk: " + mlOfMilk + "ml");
+    System.out.println("Pumpkin Spice: " + mlOfpumpkingSpice + "ml");
   }
 }
