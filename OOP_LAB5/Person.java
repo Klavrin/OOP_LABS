9c0419795b8adc858c9f77297f4d29d13bdd9dc6
Sergiu Gherasim <sergeras4@gmail.com>
2025-12-02 09:18:01 +0200

    feat: add Person class

---
+++ OOP_LAB5/Person.java
@@ -0,0 +1,13 @@
+public class Person {
+  String name;
+  // String preferredCoffee;
+  // Intensity preferrenedIntensity;
+
+  public Person(String name) {
+    this.name = name;
+  }
+
+  public String getName() {
+    return name;
+  }
+}
