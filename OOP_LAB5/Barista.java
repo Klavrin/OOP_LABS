0d5bcc7d6eb7bea3170677611b6c4f8097df1832
Sergiu Gherasim <sergeras4@gmail.com>
2025-12-02 10:23:01 +0200

    feat: ask for more attributes in Barista class

--- OOP_LAB5/Barista.java
+++ OOP_LAB5/Barista.java
@@ -16,11 +16,21 @@ public class Barista extends Person {
       if (coffee instanceof Americano americano) {
         System.out.println("How much water do you want in your Americano?");
         int ml = myObj.nextInt();
+
+        System.out.println("How intense do you want your Americano to be? (0. LIGHT, 1. MEDIUM, 2. STRONG)");
+        Intensity intensity = Intensity.values()[myObj.nextInt()];
+
+        americano.setIntensity(intensity);
         americano.setMlOfWater(ml);
         americano.makeAmericano();
       } else if (coffee instanceof Cappuccino cappuccino) {
         System.out.println("How much milk do you want in your Americano?");
         int ml = myObj.nextInt();
+
+        System.out.println("How intense do you want your Cappuccino to be? (0. LIGHT, 1. MEDIUM, 2. STRONG)");
+        Intensity intensity = Intensity.values()[myObj.nextInt()];
+
+        cappuccino.setIntensity(intensity);
         cappuccino.setMlOfMilk(ml);
         cappuccino.makeCappuccino();
       } else if (coffee instanceof PumpkinSpiceLatte pumpkinSpiceLatte) {
@@ -28,7 +38,10 @@ public class Barista extends Person {
         int ml = myObj.nextInt();
         System.out.println("How much pupkin spice do you want in your Pumpkin Spice Latte?");
         int grams = myObj.nextInt();
+        System.out.println("How intense do you want your Pumpkin Spice Latte to be? (0. LIGHT, 1. MEDIUM, 2. STRONG)");
+        Intensity intensity = Intensity.values()[myObj.nextInt()];
 
+        pumpkinSpiceLatte.setIntensity(intensity);
         pumpkinSpiceLatte.setMlOfMilk(ml);
         pumpkinSpiceLatte.setMlOfPumpkingSpice(grams);
         pumpkinSpiceLatte.makePumpkinSpiceLatte();
@@ -44,6 +57,10 @@ public class Barista extends Person {
         int choice = myObj.nextInt();
         SyrupType syrup = SyrupType.values()[choice];
 
+        System.out.println("How intense do you want your Syrup Cappuccino to be? (0. LIGHT, 1. MEDIUM, 2. STRONG)");
+        Intensity intensity = Intensity.values()[myObj.nextInt()];
+
+        syrupCappuccino.setIntensity(intensity);
         syrupCappuccino.setMlOfMilk(ml);
         syrupCappuccino.setSyrup(syrup);
         syrupCappuccino.makeSyrupCappuccino();
