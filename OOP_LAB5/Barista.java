87b4b2e28f40bcc85eae079cf378ac58baa36b76
Sergiu Gherasim <sergeras4@gmail.com>
2025-12-02 10:04:38 +0200

    feat: add Barista class

---
+++ OOP_LAB5/Barista.java
@@ -0,0 +1,54 @@
+import java.util.List;
+import java.util.Scanner;
+
+public class Barista extends Person {
+  List<Coffee> listOfCoffees;
+  
+  public Barista(String name, List<Coffee> listOCoffees) {
+    super(name);
+    this.listOfCoffees = listOCoffees;
+  }
+
+  public void prepareCoffee(String coffeeName) {
+    for (Coffee coffee : listOfCoffees) {
+      Scanner myObj = new Scanner(System.in);
+
+      if (coffee instanceof Americano americano) {
+        System.out.println("How much water do you want in your Americano?");
+        int ml = myObj.nextInt();
+        americano.setMlOfWater(ml);
+        americano.makeAmericano();
+      } else if (coffee instanceof Cappuccino cappuccino) {
+        System.out.println("How much milk do you want in your Americano?");
+        int ml = myObj.nextInt();
+        cappuccino.setMlOfMilk(ml);
+        cappuccino.makeCappuccino();
+      } else if (coffee instanceof PumpkinSpiceLatte pumpkinSpiceLatte) {
+        System.out.println("How much milk do you want in your Pumpkin Spice Latte?");
+        int ml = myObj.nextInt();
+        System.out.println("How much pupkin spice do you want in your Pumpkin Spice Latte?");
+        int grams = myObj.nextInt();
+
+        pumpkinSpiceLatte.setMlOfMilk(ml);
+        pumpkinSpiceLatte.setMlOfPumpkingSpice(grams);
+        pumpkinSpiceLatte.makePumpkinSpiceLatte();
+      } else if (coffee instanceof SyrupCappuccino syrupCappuccino) {
+        System.out.println("How much milk do you want in your Syrup Cappuccino?");
+        int ml = myObj.nextInt();
+
+        System.out.println("Select syrup type:");
+        for (SyrupType type : SyrupType.values()) {
+            System.out.println(type.ordinal() + " - " + type.name());
+        }
+
+        int choice = myObj.nextInt();
+        SyrupType syrup = SyrupType.values()[choice];
+
+        syrupCappuccino.setMlOfMilk(ml);
+        syrupCappuccino.setSyrup(syrup);
+        syrupCappuccino.makeSyrupCappuccino();
+      }
+      System.out.println();
+    }
+  }
+}
